f2f1e13737c8f3d5db102bdd0786588235d8c559
Adib Saikali <asaikali@gmail.com>
2020-10-12 23:07:05 -0400

    Polish

--- util/src/test/java/com/example/util/KeyExchangeTest.java
+++ util/src/test/java/com/example/util/KeyExchangeTest.java
@@ -13,8 +13,21 @@ class KeyExchangeTest {
 
     var key1 = alice.establishAes256bitKey(bob.getPublicKey());
     var key2 = bob.establishAes256bitKey(alice.getPublicKey());
+    var key3 = alice.establishAes256bitKey(bob.getPublicKey());
+    var key4 = bob.establishAes256bitKey(alice.getPublicKey());
 
     assertThat(key1).isEqualTo(key2);
     assertThat(key1.length).isEqualTo(32);
+    assertThat(key1).isEqualTo(key3);
+    assertThat(key3).isEqualTo(key4);
+
+    alice = new KeyExchange();
+    bob = new KeyExchange();
+
+    key3 = alice.establishAes256bitKey(bob.getPublicKey());
+    key4 = bob.establishAes256bitKey(alice.getPublicKey());
+
+    assertThat(key1).isNotEqualTo(key3);
+    assertThat(key3).isEqualTo(key4);
   }
 }
