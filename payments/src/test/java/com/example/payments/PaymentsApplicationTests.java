e0c5433b9b56245c8a82f2b8aa21042fbb4afa9e
Adib Saikali <asaikali@gmail.com>
2020-10-19 12:14:50 -0400

    Polish

--- payments/src/test/java/com/example/payments/PaymentsApplicationTests.java
+++ payments/src/test/java/com/example/payments/PaymentsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.payments;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(args = "--refundsPath=../data/refunds.jwe")
+@SpringBootTest
 class PaymentsApplicationTests {
 
   @Test
